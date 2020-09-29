c3d943d61d01585236a8c254e65acce6550fad93
Samuel W. Flint <swflint@flintfam.org>
2020-09-29 16:25:08 -0500

    Add initial contents

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A custom synchronization tool";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        packages = rec {
+
+          # sync-it = pkgs.rustPlatform.buildRustPackage rec {
+          #   name = "sync-it";
+          #   version = "0.0.1";
+
+          #   cargoSha256 = "XXX";
+
+          #   meta = with pkgs.stdenv.lib; {
+          #     description = "A simple, customizable synchronization tool.";
+          #     license = licenses.gpl3OrLater;
+          #     maintainers = with maintainers; [ swflint ];
+          #   };
+          # };
+
+          devEnvironment = pkgs.mkShell {
+            name = "sync-it-dev-environment";
+
+            buildInputs = [
+              pkgs.pre-commit
+              pkgs.rustc
+              pkgs.cargo
+            ];
+          };
+
+        };
+
+        devShell = packages.devEnvironment;
+      }
+    );
+}
