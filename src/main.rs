3613c0897a4e808c440ff4e08a713821d9e9ee60
Samuel W. Flint <swflint@flintfam.org>
2020-09-29 20:37:31 -0500

    Add start of CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
+mod run;
+use run::run;
+
+mod config;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    match matches.subcommand_name() {
+        Some("run") => if let Some(matches) = matches.subcommand_matches("run") {
+            let names = matches.values_of("name").unwrap();
+            // names.mambo
+            // println!();
+            run(names);
+            // for thing in names {}
+        },
+        _ => println!("Nothing")
+    }
+    
 }
