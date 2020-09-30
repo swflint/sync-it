a883d13f3473c51d9ceb06013a6099ce800d80ce
Samuel W. Flint <swflint@flintfam.org>
2020-09-30 11:50:20 -0500

    Drop some old stuff until it's written

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@
 extern crate clap;
 use clap::App;
 
-mod run;
-use run::run;
-
 mod config;
 
 fn main() {
@@ -12,14 +9,14 @@ fn main() {
     let matches = App::from_yaml(yaml).get_matches();
 
     match matches.subcommand_name() {
-        Some("run") => if let Some(matches) = matches.subcommand_matches("run") {
-            let names = matches.values_of("name").unwrap();
-            // names.mambo
-            // println!();
-            run(names);
-            // for thing in names {}
-        },
-        _ => println!("Nothing")
+        Some("run") => println!("Running..."), // if let Some(matches) = matches.subcommand_matches("run") {
+        //     let names = matches.values_of("name").unwrap();
+        //     // names.mambo
+        //     // println!();
+        //     run(names);
+        //     // for thing in names {}
+        // },
+        Some(thing) => println!("{}", thing),
+        _ => println!("No subcommand."),
     }
-    
 }
