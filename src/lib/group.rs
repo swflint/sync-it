f7d138235701bc543a1a91c1ae8d7091e2385807
Samuel W. Flint <swflint@flintfam.org>
2020-09-30 19:52:59 -0500

    Implemented formatting in groups

--- src/lib/group.rs
+++ src/lib/group.rs
@@ -11,6 +11,16 @@ pub struct Group {
     members: Vec<String>,
 }
 
-/*impl fmt::Display for Group {
-    
-}*/
+impl fmt::Display for Group {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Group {}:\n\tRepos:\n", self.name)?;
+        for repo in &self.members {
+            write!(f, "\t\t - {}\n", repo)?;
+        }
+        write!(f, "\tActions\n")?;
+        for action in &self.actions_after {
+            write!(f, "\t\t - {}\n", action)?;
+        }
+        write!(f, "")
+    }
+}
